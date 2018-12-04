26072c4950e2133a0211327504ca3354582433ea
Jan Rádl <decent_descent@Decent-Descent.local>
2018-12-04 12:52:20 +0100

    CSS Nano and Autoprefixer

--- next.config.js
+++ next.config.js
@@ -1,2 +1,16 @@
+const withPlugins = require("next-compose-plugins");
+const withCSS = require("@zeit/next-css");
 const withSass = require("@zeit/next-sass");
-module.exports = withSass();
+
+const nextConfig = {
+  webpack: config => {
+    config.node = {
+      fs: "empty",
+      modules: false
+    };
+
+    return config;
+  }
+};
+
+module.exports = withPlugins([withCSS, withSass], nextConfig);
